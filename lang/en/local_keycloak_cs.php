459533286d8888755d1131bdf6c11ade7aef6481
zalviandyr <zukronalviandy@gmail.com>
2024-10-02 14:32:48 +0700

    feat: update keycloak

--- lang/en/local_keycloak_cs.php
+++ lang/en/local_keycloak_cs.php
@@ -1,12 +1,4 @@
 <?php
 defined('MOODLE_INTERNAL') || die();
 
-$string['pluginname'] = 'Keycloak CS - Mochammad Rafi';
-$string['realm'] = 'Realm';
-$string['realm_desc'] = 'The realm for the Keycloak authentication';
-$string['client_id'] = 'Client ID';
-$string['client_id_desc'] = 'The client ID used to authenticate with Keycloak';
-$string['keycloak_url'] = 'Keycloak URL';
-$string['keycloak_url_desc'] = 'The base URL for your Keycloak instance';
-$string['silent_redirect_url'] = 'Your Silent Redirect URL';
-$string['silent_redirect_url_desc'] = 'Example. https://your.site/my';
+$string['pluginname'] = 'Keycloak CS - ZalviandyR';
--- lib.php
+++ lib.php
@@ -2,13 +2,25 @@
 defined('MOODLE_INTERNAL') || die();
 
 function local_keycloak_cs_extend_navigation(\navigation_node $nav) {
-    global $PAGE, $USER, $CFG, $OUTPUT;
+    global $PAGE, $USER, $CFG, $OUTPUT, $DB;
 
-    $html = $OUTPUT->render_from_template('local_keycloak_cs/keycloak_cs', [
-        'keycloak_url' => get_config('local_keycloak_cs', 'keycloak_url'),
-        'client_id' => get_config('local_keycloak_cs', 'client_id'),
-        'realm' => get_config('local_keycloak_cs', 'realm'),
-        'is_logged_in' => $USER->id !== 0 ? 'true' : 'false',
-    ]);
-    echo $html;
+    $issuer = $DB->get_record_select('oauth2_issuer', 'enabled = 1');
+    if ($issuer) {
+        $baseUrl = $issuer->baseurl;
+        $baseUrl = explode('/realms/', $baseUrl);
+
+        $issuerId = $issuer->id;
+        $issuerClient = $issuer->clientid;
+        $issuerRealm = $baseUrl[1];
+        $issuerUrl = $baseUrl[0];
+
+        $html = $OUTPUT->render_from_template('local_keycloak_cs/keycloak_cs', [
+            'issuer_id' => $issuerId,
+            'keycloak_url' => $issuerUrl,
+            'client_id' => $issuerClient,
+            'realm' => $issuerRealm,
+            'is_logged_in' => $USER->id !== 0 ? 'true' : 'false',
+        ]);
+        echo $html;
+    }
 }
